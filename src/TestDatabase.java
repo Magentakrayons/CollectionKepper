69cc875ec0f0672daa884e7ede07ff6024457763
Mark O <mark.olegario2014@gmail.com>
2018-03-22 21:09:31 -0400

    Update TestDatabase.java
    Added more demo test cases.

--- src/TestDatabase.java
+++ src/TestDatabase.java
@@ -9,17 +9,44 @@ public class TestDatabase {
 	public static void main(String[] args) {
 		TestDatabase testdb = new TestDatabase();
 		ArrayList<ArrayList<String>> database = testdb.CreateDatabase();
+		SearchAlgorithms searchfuncs = new SearchAlgorithms();
 		
 		//Index 0, alphbetical
-		SearchAlgorithms searchfuncs = new SearchAlgorithms();
+		System.out.println("Alphabetical Sort: Column 0");
 		searchfuncs.sortAlpha(database, 0);
 		for (ArrayList<String> s : database) {
 			System.out.println(s);
 		}
-		//Reverse order
 		System.out.println();
+
+		//Reverse order
+		System.out.println("Reverse Column 0");
 		searchfuncs.reverseOrder(database);
 		for (ArrayList<String> s : database) {
+			System.out.println(s);
+		}
+		System.out.println();
+		
+		//Index 4, numeric
+		System.out.println("Alphabetical Sort: Column 4");
+		searchfuncs.sortNumeric(database, 4);
+		for (ArrayList<String> s : database) {
+			System.out.println(s);
+		}
+		System.out.println();
+		
+		//Search for any instances of 'd' in database column 2
+		System.out.println("Searching column 3: 'd'");
+		ArrayList<ArrayList<String>> newdb = searchfuncs.searchEntry(database, "d", 2);
+		for (ArrayList<String> s : newdb) {
+			System.out.println(s);
+		}
+		System.out.println();
+		
+		//Search for any instance of '70' in database column 3
+		System.out.println("Searching column 4: '70'");
+		newdb = searchfuncs.searchEntry(database, "70", 3);
+		for (ArrayList<String> s : newdb) {
 			System.out.println(s);
 		}
 	}
