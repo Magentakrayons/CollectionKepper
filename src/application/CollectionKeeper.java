845fa0988c6dc0f41ba0393b6b0a4785b9b3deff
Mark O <mark.olegario2014@gmail.com>
2018-04-22 00:59:54 -0400

    Extracted methods to new classes
    All functions activated have been extracted to new classes.

--- src/application/CollectionKeeper.java
+++ src/application/CollectionKeeper.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,6 +12,8 @@ import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -39,18 +42,27 @@ public class CollectionKeeper extends Application {
 
 	//Use for master list
 	public static ObservableList<ObservableList<Object>> database = FXCollections.observableArrayList();
+	//not hooked up yet
+	public static FilteredList<ObservableList<Object>> searchDatabase = new FilteredList<>(database, p -> true);
 	//Use for column headers
 	public static ObservableList<String> categoryList = FXCollections.observableArrayList();
 	public static TableView<ObservableList<Object>> mainTable = new TableView();
 
+	private DatabaseHandler databaseManager = new DatabaseHandler();
+	private EntryHandler entryHandler = new EntryHandler();
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			/*
+			 * Begin setting up primaryStage.
+			 */
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,900,600);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Collection Keeper");
+
+			//Causes all windows to close if primaryStage is closed
+			primaryStage.setOnHidden(e -> Platform.exit());
 
 			/*
 			 * Creation of menuBar and buttons begins here.
@@ -77,10 +89,16 @@ public class CollectionKeeper extends Application {
 			menuFile.getItems().addAll(menuItemCreateDatabase, menuItemLoadDatabase, menuItemSaveDatabase, menuItemExit);
 
 			//Edit Menu
-			Menu menuEdit = new Menu("Edit");
-			MenuItem menuItemEditEntry = new MenuItem("Edit Entry");
-			menuItemEditEntry.setOnAction(menuController);
-			menuEdit.getItems().add(menuItemEditEntry);
+			Button buttonEdit = new Button("Edit");
+			buttonEdit.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
+			buttonEdit.setOnAction(buttonController);
+			buttonEdit.setAlignment(Pos.TOP_RIGHT);
+
+			//Delete Item
+			Button buttonDelete = new Button("Delete");
+			buttonDelete.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
+			buttonDelete.setOnAction(buttonController);
+			buttonDelete.setAlignment(Pos.TOP_RIGHT);
 
 			//Add New Item
 			Button buttonAddNew = new Button("Add New");
@@ -100,17 +118,50 @@ public class CollectionKeeper extends Application {
 			buttonMinusOne.setOnAction(buttonController);
 			buttonPlusOne.setAlignment(Pos.TOP_RIGHT);
 
+			/*
+			 * Setup of mainTable filtering begins here.
+			 */
+
+			//Set up FilterField
+			TextField searchBar = new TextField();
+			searchBar.setMinSize(200, TextField.USE_PREF_SIZE);
+			searchBar.setAlignment(Pos.TOP_CENTER);
+			searchBar.textProperty().addListener((observable, oldValue, newValue) -> {
+				searchDatabase.setPredicate(Object -> {
+					//If Filter text is empty, display all items
+					if (newValue == null || newValue.isEmpty()) {
+						System.out.println("now empty");
+						return true;
+					}
+					String filterText = String.valueOf(searchBar.getText()).toLowerCase();
+					//compare filtered text to all entries
+					for (ObservableList<Object> entry : database) {
+						if(String.valueOf(entry).toLowerCase().contains(filterText)) {
+							return true; // Filter matches
+						}
+					}
+					return false;
+				});
+			});
+
+			/*
+			 * Finishing configuration of primaryStage begins here.
+			 */
+
 			//Add all items to Top of BorderPane
-			HBox hBox = new HBox(menuBar, buttonAddNew, buttonPlusOne, buttonMinusOne);
+			HBox hBox = new HBox(menuBar, searchBar, buttonEdit, buttonDelete, buttonAddNew, buttonPlusOne, buttonMinusOne);
 			hBox.setHgrow(menuBar, Priority.ALWAYS);
+			hBox.setHgrow(searchBar, Priority.ALWAYS);
+			hBox.setHgrow(buttonEdit, Priority.NEVER);
+			hBox.setHgrow(buttonDelete, Priority.NEVER);
+			hBox.setHgrow(buttonAddNew, Priority.NEVER);
 			hBox.setHgrow(buttonPlusOne, Priority.NEVER);
 			hBox.setHgrow(buttonMinusOne, Priority.NEVER);
 			root.setTop(hBox);
-			menuBar.getMenus().addAll(menuFile, menuEdit);
+			menuBar.getMenus().addAll(menuFile);
 
 			root.setCenter(mainTable);
 
-			//adds cute girl to the stage : ^)
 			primaryStage.getIcons().add(new Image("mahira.jpg"));
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -120,10 +171,16 @@ public class CollectionKeeper extends Application {
 	}
 
 	public static void main(String[] args) {
+		//Creates a new Database Directory if one does not exist.
+		File saveDir = new File("Databases/");
+		boolean exists = saveDir.exists();
+		if (!exists) {
+			saveDir.mkdir();
+		}
+		//Begins creating main window.
 		launch(args);
 	}
 
-	//Println stubs need to be implemented
 	//Handler for MenuBar
 	private EventHandler<ActionEvent> MenuBarController()  {
 		return new EventHandler<ActionEvent>() {
@@ -134,35 +191,43 @@ public class CollectionKeeper extends Application {
 
 				//Handle event
 				if ("create".equalsIgnoreCase(text)) {
-					System.out.println("create selected");
+					databaseManager.createNewDatabase();
 				} else if ("load".equalsIgnoreCase(text)) {
-					//refreshes working database and categories to empty
-					database.clear();
-					categoryList.clear();
-					mainTable.getItems().clear();
-					mainTable.getColumns().clear();
+					//load database method
+					ObservableList<ObservableList<Object>> tempDatabase = DatabaseHandler.loadDatabase();
 
-					//test database method
-					database = createDatabase();
+					//determines whether to rebuild database.
+					if (tempDatabase != null) {
 
+						//refreshes working database and categories to empty
+						database.clear();
+						mainTable.getItems().clear();
+						mainTable.getColumns().clear();
+						database = tempDatabase;
 
-					for (int i = 0; i < database.get(0).size(); i++) {
-						final int colCount = i;
-						final TableColumn<ObservableList<Object>, Object> column = new TableColumn<>(String.valueOf(categoryList.get(i)));
-						column.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue().get(colCount)));
-						mainTable.getColumns().add(column);
+						for (int i = 0; i < database.get(0).size(); i++) {
+							final int colCount = i;
+							final TableColumn<ObservableList<Object>, Object> column = new TableColumn<>(String.valueOf(categoryList.get(i)));
+							column.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue().get(colCount)));
+							mainTable.getColumns().add(column);
 
-					}
-					for (int i = 0; i < database.size(); i++) {
-						mainTable.getItems().add(database.get(i));
+						}
+						mainTable.setItems(database);
+
+						/*for (int i = 0; i < database.size(); i++) {
+							mainTable.getItems().add(database.get(i));
+						}
+						*/
+
 					}
 				} else if ("save".equalsIgnoreCase(text)) {
-					System.out.println("save selected");
+					try {
+						databaseManager.saveDatabase();
+					} catch (IOException e) {
+						//pass
+					}
 				} else if ("exit".equalsIgnoreCase(text)) {
 					Platform.exit();
-				} else if ("edit entry".equalsIgnoreCase(text)) {
-					int index = database.indexOf(mainTable.getSelectionModel().getSelectedItem());
-					editEntry(database.get(index));
 				}
 			}
 		};
@@ -178,132 +243,41 @@ public class CollectionKeeper extends Application {
 				String text = item.getText();
 
 				//Handle event
-				if ("add new".equalsIgnoreCase(text)) {
-					System.out.println("add new selected");
-					//debugging lines
-					System.out.println(database);
-					System.out.println(mainTable.getSelectionModel().getSelectedItem());
-					System.out.println(database.indexOf(mainTable.getSelectionModel().getSelectedItem()));
-				}
-				if ("+1".equalsIgnoreCase(text)) {
-					int index = database.indexOf(mainTable.getSelectionModel().getSelectedItem());
-					int count = (int) database.get(index).get(0) + 1;
-					database.get(index).set(0, count);
-					mainTable.refresh();
+				if ("edit".equalsIgnoreCase(text)) {
+					try {
+						int index = database.indexOf(mainTable.getSelectionModel().getSelectedItem());
+						entryHandler.editEntry(database.get(index));
+					} catch (ArrayIndexOutOfBoundsException e) {
+						//pass
+					}
+				} else if ("delete".equalsIgnoreCase(text)) {
+					try {
+						entryHandler.deleteEntry();
+					} catch (ArrayIndexOutOfBoundsException e) {
+						//pass
+					}
+				} else if ("add new".equalsIgnoreCase(text)) {
+					entryHandler.addEntry();
+				} else if ("+1".equalsIgnoreCase(text)) {
+					try {
+						int index = database.indexOf(mainTable.getSelectionModel().getSelectedItem());
+						int count = (int) database.get(index).get(0) + 1;
+						database.get(index).set(0, count);
+						mainTable.refresh();
+					} catch (ArrayIndexOutOfBoundsException e) {
+						//pass
+					}
 				} else if ("-1".equalsIgnoreCase(text)) {
-					int index = database.indexOf(mainTable.getSelectionModel().getSelectedItem());
-					int count = (int) database.get(index).get(0) - 1;
-					database.get(index).set(0, count);
-					mainTable.refresh();
+					try {
+						int index = database.indexOf(mainTable.getSelectionModel().getSelectedItem());
+						int count = (int) database.get(index).get(0) - 1;
+						database.get(index).set(0, count);
+						mainTable.refresh();
+					} catch (ArrayIndexOutOfBoundsException e) {
+						//pass
+					}
 				}
 			}
 		};
 	}
-
-	//Edits an entry in the table
-	private void editEntry(ObservableList<Object> entry) {
-		//build window
-		Stage editStage = new Stage();
-		editStage.setTitle("Edit Entry");
-		editStage.getIcons().add(new Image("mahira.jpg"));
-		GridPane editPane = new GridPane();
-		editPane.setHgap(50);
-		editPane.setVgap(5);
-		editPane.setPadding(new Insets(5,5,5,5));
-		Scene scene = new Scene(editPane, 250, (30*entry.size() + 70));
-
-
-		//Populate Grid
-		//Make categories
-		for (int i = 0; i < database.get(0).size(); i++) {
-			Label regex = new Label(categoryList.get(i));
-			editPane.add(regex, 0, i);
-		}
-		//Make Textfields
-		ObservableList<TextField> textBoxList = FXCollections.observableArrayList();
-		for (int i = 0; i < entry.size(); i++) {
-			String text = String.valueOf(entry.get(i));
-			TextField textField = new TextField(text);
-			editPane.add(textField, 1, i);
-			textBoxList.add(textField);
-		}
-
-		//add confirm button and method for handling confirmed changes
-		Button confirmButton = new Button("Confirm Changes");
-		confirmButton.setMinSize(40, 25);
-		confirmButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				for (int i = 0; i < textBoxList.size(); i++) {
-					try { //writes as int if all numbers
-						int convertedInt = Integer.parseInt((String) textBoxList.get(i).getText());
-						entry.set(i, convertedInt);
-					} catch (NumberFormatException e) { // writes as string if has non-int characters
-						entry.set(i, textBoxList.get(i).getText());
-					}
-				}
-				editStage.hide();
-				mainTable.refresh();
-			}
-		});
-
-
-		//finalize window
-		editPane.add(confirmButton, 1, editPane.getChildren().size());
-		editPane.setAlignment(Pos.TOP_CENTER);
-
-		editStage.setScene(scene);
-		editStage.show();
-	}
-
-
-	//Add new Entry
-
-
-
-
-	//This method exists for testing database with table. Please remove it later.
-	public static ObservableList createDatabase() {
-		// TODO Auto-generated method stub
-	File file = new File("testData.txt");
-	try {
-		Scanner s = new Scanner(file);
-		ObservableList<ObservableList<Object>> db = FXCollections.observableArrayList();
-		boolean firstpass = true;
-		while (s.hasNextLine()) {
-			String text = s.nextLine();
-			String[] tokens = text.split("/");
-			ObservableList<Object> tempList = FXCollections.observableArrayList();
-			for(String item: tokens) {
-				tempList.add(item);
-			}
-			if (firstpass) {
-				for (Object category : tempList) {
-					categoryList.add(category.toString());
-				}
-				firstpass = false;
-			} else {
-				db.add(tempList);
-			}
-		}
-
-		//fix numbers to ints
-		for (int i = 0; i < db.size(); i++) {
-			for (int j = 0; j < categoryList.size(); j++) {
-				try {
-					int convertedInt = Integer.parseInt((String) db.get(i).get(j));
-					db.get(i).set(j, convertedInt);
-				} catch (NumberFormatException e) {
-					//pass
-				}
-			}
-		}
-		return db;
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	return null;
-	}
-
 }
